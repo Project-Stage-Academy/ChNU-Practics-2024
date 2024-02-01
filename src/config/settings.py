86e19535b3bbac2dbcabd0d80f830785b3f8b1cc
Ivan Tkachuk <git@tkachuk.email>
2024-02-01 21:54:45 +0200

    Add rest_framework to django project

--- src/config/settings.py
+++ src/config/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework", # Django REST Framework
 ]
 
 MIDDLEWARE = [
@@ -105,3 +106,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+
+}
