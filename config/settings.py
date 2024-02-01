779abc89dcc4cab4d8f08494a9dec5e2e4c982b3
Ivan Tkachuk <git@tkachuk.email>
2024-02-01 20:26:40 +0200

    Add environment variables and update settings.py to load env

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,13 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-^s661*i5m_$31pm!-=99_1^gaf#0ot8!@&oq-gfa-)b%zrsoqk"
-
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 
+ENV_ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS")
 ALLOWED_HOSTS = []
-
+if ENV_ALLOWED_HOSTS:
+    ALLOWED_HOSTS = [ENV_ALLOWED_HOSTS]
 
 # Application definition
 
