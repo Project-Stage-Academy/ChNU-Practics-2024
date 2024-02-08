15bdff8caf11eee23b4aee2d4d77f797d9e92bbd
Ivan Tkachuk <git@tkachuk.email>
2024-02-08 19:44:10 +0200

    Format code

--- src/apps/api/v1/urls.py
+++ src/apps/api/v1/urls.py
@@ -1,9 +1,8 @@
 from django.urls import include
 from django.urls import path
-
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from apps.users.views import UserRegisterView
 
@@ -12,7 +11,7 @@ router = DefaultRouter()
 
 urlpatterns = [
     path("", include(router.urls)),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path(r"auth/register/", UserRegisterView.as_view(), name="user-register"),
 ]
