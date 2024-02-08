c4f26f5022bd2e5724fad0597a7ec40320541cd4
Ivan Tkachuk <git@tkachuk.email>
2024-02-08 18:13:07 +0200

    Add script for createsuperuser and migrate db

---
+++ migrate.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+POSTGRES_HOST=${POSTGRES_HOST:-"postgres"}
+POSTGRES_PORT=${POSTGRES_PORT:-"5432"}
+
+cd /app/
+
+sleep 2 # wait for postgres to start
+
+PYTHON_BIN=/opt/venv/bin/python
+
+$PYTHON_BIN src/manage.py createsuperuser --noinput || true
+
+$PYTHON_BIN src/manage.py makemigrations --noinput
+$PYTHON_BIN src/manage.py migrate --noinput
