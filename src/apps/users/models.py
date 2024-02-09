bab1c16e74546d6fba1b228acdba3546b05d9579
Ivan Tkachuk <git@tkachuk.email>
2024-02-09 13:46:39 +0200

    Add to User PermissionsMixin and make default for auth

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -2,10 +2,13 @@ import uuid
 
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
+from django.utils import timezone
 
 
 class UserRole(models.TextChoices):
+    ADMIN = "admin", "Admin"
     INVESTOR = "investor", "Investor"
     STARTUP = "startup", "Startup"
 
@@ -31,11 +34,18 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_verified", True)
+        extra_fields.setdefault("role", UserRole.ADMIN)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
 
         return self.create_user(email, password, **extra_fields)
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = "email"
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -50,6 +60,13 @@ class User(AbstractBaseUser):
 
     first_name = models.CharField("First Name", max_length=15, blank=True, null=True)
     last_name = models.CharField("Last Name", max_length=15, blank=True, null=True)
+
+    is_active = models.BooleanField("Active", default=True)
+    is_staff = models.BooleanField("Staff Status", default=False)
+    is_superuser = models.BooleanField("Superuser Status", default=False)
+    is_verified = models.BooleanField("Verified", default=False)
+
+    date_joined = models.DateTimeField("Date Joined", default=timezone.now)
 
     objects: CustomUserManager = CustomUserManager()
 
