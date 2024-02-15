c5473bd38837739a622b23dddb8eb3f87fb1f490
Ivan Tkachuk <116971836+ivtka@users.noreply.github.com>
2024-02-15 10:57:40 +0200

    Implementing Password Recovery (#38)
    Fixes #31

--- src/apps/authentication/urls.py
+++ src/apps/authentication/urls.py
@@ -3,6 +3,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework_simplejwt.views import TokenVerifyView
 
+from .views import PasswordRecoveyView
+from .views import PasswordResetView
 from .views import RegisterView
 from .views import VerifyEmailView
 
@@ -13,4 +15,8 @@ urlpatterns = [
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path(r"register/", RegisterView.as_view(), name="register"),
     path(r"email/verify/", VerifyEmailView.as_view(), name="verify-email"),
+    path(
+        r"password/recovery/", PasswordRecoveyView.as_view(), name="password-recovery"
+    ),
+    path(r"password/reset/", PasswordResetView.as_view(), name="password-reset"),
 ]
--- src/apps/authentication/views.py
+++ src/apps/authentication/views.py
@@ -4,17 +4,18 @@ from rest_framework import status
 from rest_framework.exceptions import APIException
 from rest_framework.response import Response
 
+from .serializers import PasswordRecoverySerializer
+from .serializers import PasswordResetSerializer
 from .serializers import RegisterSerializer
-from .utils import get_user_from_token
+from .utils import decode_token
 from .utils import send_confirmation_email
+from .utils import send_password_recovery_email
+
 
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
+    
     def perform_create(self, serializer):
         user = serializer.save()
         send_confirmation_email(self.request, user)
@@ -24,23 +25,48 @@ class VerifyEmailView(generics.GenericAPIView):
     def get(self, request, *args, **kwargs):
         token = request.query_params.get("token")
 
-        try:
-            user = get_user_from_token(token)
-            self.verify_user_email(user)
+        user = decode_token(token)
+        self.verify_user_email(user)
 
-            return Response(
-                {"message": "Email verified successfully"}, status=status.HTTP_200_OK
-            )
+        return Response(
+            {"message": "Email verified successfully"}, status=status.HTTP_200_OK
+        )
 
-        except jwt.ExpiredSignatureError:
-            raise APIException(
-                detail="Token expired", code=status.HTTP_400_BAD_REQUEST
-            ) from None
-        except jwt.DecodeError:
-            raise APIException(
-                detail="Token invalid", code=status.HTTP_400_BAD_REQUEST
-            ) from None
 
     def verify_user_email(self, user):
         user.is_verified = True
         user.save()
+
+
+class PasswordRecoveyView(generics.GenericAPIView):
+    serializer_class = PasswordRecoverySerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+
+        send_password_recovery_email(request, user)
+
+        return Response(
+            {"message": "Password recovery email sent successfully"},
+            status=status.HTTP_200_OK,
+        )
+
+
+class PasswordResetView(generics.CreateAPIView):
+    serializer_class = PasswordResetSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        token = request.query_params.get("token")
+        user = decode_token(token)
+
+        user.set_password(serializer.validated_data["new_password"])
+        user.save()
+
+        return Response(
+            {"message": "Password reset successfully"}, status=status.HTTP_200_OK
+        )
