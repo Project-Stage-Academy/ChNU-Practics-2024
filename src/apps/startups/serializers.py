d623afe335ddc56ba08149f889fb581bba8c2e67
Adeliona <adenosine382@gmail.com>
2024-02-20 19:51:47 +0200

    add template for view profile

--- src/apps/startups/serializers.py
+++ src/apps/startups/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 
 from .models import Startup
+from apps.users.serializers import FounderSerializer
 
 
 class StartupSerializer(serializers.ModelSerializer):
+    founders = FounderSerializer(many=True)
     class Meta:
         model = Startup
         fields = "__all__"
