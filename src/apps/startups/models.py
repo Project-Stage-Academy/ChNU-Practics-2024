94eb678b990ecbfc9eafee204ccdab096f59aa0d
Adeliona <103882171+Adeliona@users.noreply.github.com>
2024-02-27 13:51:00 +0200

    Implement view startup profile (#43)

---
+++ src/apps/startups/models.py
@@ -0,0 +1,46 @@
+import uuid
+
+from django.core.validators import RegexValidator
+from django.db import models
+from django.utils import timezone
+
+from apps.users.models import User, Founder
+
+
+# from pictures.models import PictureField
+
+
+class Startup(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    company_name = models.CharField("Company Name", max_length=64)
+
+    founders = models.ManyToManyField(Founder)
+
+    STARTUP_SIZE = (("S", "Small"), ("M", "Medium"), ("B", "Big"), ("L", "Large"))
+
+    size = models.CharField(
+        max_length=1,
+        choices=STARTUP_SIZE,
+        default="S",
+        help_text="Startup size",
+    )
+
+    phone_number = models.CharField(
+        max_length=16,
+        blank=False,
+        null=False,
+        validators=[
+            RegexValidator(
+                regex=r"^[\+]?[(]?[0-9]{3}[)]?[\s\.]?[0-9]{3}[\s\.]?[0-9]{4,6}$",
+                message="Phone number must be entered in the format '+123456789'. Up to 15 digits allowed.",
+            ),
+        ],
+    )
+
+    location = models.CharField(max_length=255, blank=True)
+
+    created_at = models.DateTimeField("Created", default=timezone.now)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.company_name
---
+++ src/apps/startups/views.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, render
+from rest_framework import generics, permissions, status, viewsets
+from rest_framework.response import Response
+
+from .models import Startup
+from .serializers import (
+    StartupSerializer,
+)
+
+
+class StartupViewSet(viewsets.ModelViewSet):
+    queryset = Startup.objects.all()
+    serializer_class = StartupSerializer
+
+
+
+def startup_profile_view(request, startup_id):
+    startup = get_object_or_404(Startup, id=startup_id)
+
+    for founder in startup.founders.all():
+        print(founder)
+
+    return render(request, "startup_profile.html", {"startup": startup})
